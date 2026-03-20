012f00b7af186246e31355924f6ac1b8796423e7
Nosima <breno.nosima@gmail.com>
2026-03-20 11:49:19 -0300

    firt commit

---
+++ src/main/java/com/bn/clientecs/ClienteCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.clientecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/clientecs/ClienteCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.clientecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
