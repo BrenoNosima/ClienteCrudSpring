719abb5d063e7990fd23f83bd53838df20105c36
Nosima <breno.nosima@gmail.com>
2026-03-20 19:30:05 -0300

    model criada

---
+++ src/main/java/com/bn/clientecs/models/ClienteModel.java
@@ -0,0 +1,52 @@
+package com.bn.clientecs.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cliente")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "email")
+    private String email;
+    private String telefone;
+
+    public ClienteModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
